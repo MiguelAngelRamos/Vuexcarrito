a853d6ab8b2f97ad907a321d76f816c3d3424048
Miguel Ramos <return.infox@gmail.com>
2022-05-16 10:32:29 -0400

    creacion de la tienda

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    productos: [],
+    carrito: {}
   },
   mutations: {
+    setProductos(state, payload) {
+      state.productos = payload
+    }
   },
   actions: {
+    async getProductosApi({commit}) {
+      try {
+        const response = await fetch('api.json');
+        const data  = await response.json();
+        commit('setProductos', data);
+
+      } catch (error) {
+        throw error;
+      }
+    }
+  },
+  getters: {
   },
   modules: {
   }
