ae97644c0057d0b2165bcfd387ca4a00393672a9
Miguel Ramos <return.infox@gmail.com>
2022-05-16 12:08:08 -0400

    proyecto carrito de compras con seleccion de productos

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,22 +14,81 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
           </li>
         </ul>
+
+        <!-- icono del carrito de compras -->
+        <router-link class="carrito-container" to="/">
+
+          <div class="carrito-icon">
+            <!-- icono -->
+            <i class="fas fa-cart-plus"></i>
+          </div>
+
+          <div class="counter">
+            <div class="counter-number">
+              {{ Object.keys(carrito).length}}
+            </div>
+          </div>
+
+        </router-link>
+        <!-- fin de icono del carrito de compras -->
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+export default {
+  setup() {
+    const store = useStore();
+    const carrito = computed(() => store.state.carrito);
+
+    return {
+      carrito
+    }
+  }
+};
 </script>
 
 <style scoped>
   .navbar {
     box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.4);
   }
+
+  .carrito-container {
+    position: relative;
+  }
+
+  .carrito-icon {
+    font-size: 32px;
+    color: #37976c;
+    margin-right: 40px;
+  }
+
+  .counter {
+    position: absolute;
+    background-color: tomato;
+    color: white;
+    padding: 10px;
+    border-radius: 50%;
+    height: 32px;
+    width: 30px;
+    top: -8px;
+    left: 29px;
+  }
+
+  .counter .counter-number {
+    position: relative;
+    top: -6px;
+    /* top: -4px;
+    left: -4px; */
+  }
+
+
 </style>
